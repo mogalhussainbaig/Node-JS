ab9b93845d76cce3060ce62c0f49e29468c21642
Mogal Hussain Baig <mogalhussain.baig@karkinos.in>
2025-02-20 15:13:47 +0530

    hussain

--- database/mongo_data_base.js
+++ database/mongo_data_base.js
@@ -1,6 +1,9 @@
 const mongo=require("mongoose");
 
-const db=mongo.connect(process.env.MOGOURL).then(() => console.log('db connected'));
+const db=mongo.connect(process.env.MOGOURL,{
+     useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('db connected'));
 
 
 module.exports=db;
